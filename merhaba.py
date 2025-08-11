ad437437020d0074a2d97ea71d84eb7b43763225
zeyneppp11 <zeyneptuncpinar74@gmail.com>
2025-08-11 15:06:32 +0300

    deneme

---
+++ merhaba.py
@@ -0,0 +1,9 @@
+#mesaj="Merhaba"
+#mesaj2="Dünya"
+## print (mesaj[0]+" "+mesaj2[1]) ##
+## print(mesaj[0:4])  ##
+## print(mesaj[2:4])  ##
+## print(mesaj.upper()+" "+mesaj2.upper()) ##
+## print(mesaj.lower()+" "+mesaj2.lower()) 
+## print(mesaj.capitalize()+" "+ mesaj2.capitalize()) ##
+print ("Merhaba ")
