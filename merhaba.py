f705f5a2a4b2c37dc4fbc833b0d3ed74ab391ab4
zeyneppp11 <zeyneptuncpinar74@gmail.com>
2025-08-12 09:32:53 +0300

    denemee

--- merhaba.py
+++ merhaba.py
@@ -1,11 +1,43 @@
 #mesaj="Merhaba"
 #mesaj2="Dünya"
-## print (mesaj[0]+" "+mesaj2[1]) ##
-## print(mesaj[0:4])  ##
-## print(mesaj[2:4])  ##
-## print(mesaj.upper()+" "+mesaj2.upper()) ##
+## print (mesaj[0]+" "+mesaj2[1]) 
+## print(mesaj[0:4]) 
+## print(mesaj[2:4])  
+## print(mesaj.upper()+" "+mesaj2.upper()) 
 ## print(mesaj.lower()+" "+mesaj2.lower()) 
-## print(mesaj.capitalize()+" "+ mesaj2.capitalize()) ##
-print ("Merhaba ")
-print("DNA") 
-print("denemeee")
+## print(mesaj.capitalize()+" "+ mesaj2.capitalize())
+
+##isim ="Elif"
+##yas="20"
+##print("{} {} yaşındadır".format(isim,yas))
+
+##sayi1= 5 ** 100
+##sayi2= 21/7
+##print(sayi2)
+
+##print(5 ** 3)
+
+#renkler=["Siyah","Beyaz","Sarı","Mavi","Yeşil"]
+#print (renkler[2:]) İNDEXİ 2 DEN SONRAKİ RENKLERİ ALIR
+
+##renkler=["Kırmızı","Pembe","Mor","Turuncu","Turkuaz","Lacivert","Mavi","Lila"]
+##print (renkler [2:6])
+
+##s1=7
+##s2=99
+##print(s1>s2)
+
+#renkler=["Kırmızı","Pembe","Mor","Turuncu","Turkuaz","Lacivert","Mavi","Lila"]
+#print (renkler [:6])
+
+#renkler=["Kırmızı","Pembe","Mor","Turuncu","Turkuaz","Lacivert","Mavi","Lila"]
+#print (renkler [1:6:2])
+
+#liseler=["Fen","Anadolu","Meslek"]
+#liseler.append("Spor")
+#print(liseler).   EKLEME YAPTIK
+
+#liseler=["Fen","Anadolu","Meslek"]
+#liseler.insert  (2,"Spor")      KAÇINCI İNDEXE EKLEMEK İSTEDİĞİMİZİ YAZARIZ
+#print(liseler)
+
